dca94b90fee20c915b49393faa4f5ff58f7a3fc4
Sultan91 <erumbaevs@mail.ru>
2018-04-23 23:36:18 +0300

    poset function update

--- cup_detection/views.py
+++ cup_detection/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from cup_detection.detector import do_detection
 import glob
 import os
 # Create your views here.
 
 def index(request):
-	return render(request, 'cup_detection/index.html')
+    return render(request, 'cup_detection/index.html')
 
 def simple_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
@@ -14,11 +15,15 @@ def simple_upload(request):
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
 
+        psave = os.getcwd()+'/cup_detection/static/images/pics/'
+        do_detection(path_to_save=psave, debug=False)
+
         images_list = glob.glob('cup_detection/static/images/pics/*')
+        for i in range(len(images_list)) :
+            images_list[i] = images_list[i][len('cup_detection/static/') : len(images_list[i])]
 
         return render(request, 'cup_detection/index.html', {
             'uploaded_file_url': uploaded_file_url,
             'images_list' : images_list
         })
     return render(request, 'cup_detection/index.html')
-
