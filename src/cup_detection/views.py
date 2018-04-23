437be999dd89a7b7761acda21b51c96f0bde9784
Sultan91 <erumbaevs@mail.ru>
2018-04-24 00:12:01 +0300

    reordering imgs by name

--- src/cup_detection/views.py
+++ src/cup_detection/views.py
@@ -6,7 +6,7 @@ import os
 # Create your views here.
 
 def index(request):
-    return render(request, 'cup_detection/index.html')
+	return render(request, 'cup_detection/index.html')
 
 def simple_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
@@ -18,7 +18,8 @@ def simple_upload(request):
         psave = os.getcwd()+'/cup_detection/static/images/pics/'
         do_detection(path_to_save=psave, debug=False)
 
-        images_list = glob.glob('cup_detection/static/images/pics/*')
+        images_list = sorted(glob.glob('cup_detection/static/images/pics/*'))
+        
         for i in range(len(images_list)) :
             images_list[i] = images_list[i][len('cup_detection/static/') : len(images_list[i])]
 
